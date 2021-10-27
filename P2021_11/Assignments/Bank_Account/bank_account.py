c98d2f734af6f25543c1cf030a45c59ca2885778
Ticara <ticarab2005@yahoo.com>
2021-10-27 03:09:31 -0500

    commit

--- P2021_11/Assignments/Bank_Account/bank_account.py
+++ P2021_11/Assignments/Bank_Account/bank_account.py
@@ -1,11 +1,7 @@
-
-from User.user import User
-
-
 class BankAccount:
 
 # don't forget to add some default values for these parameters!
-    def __init__(self, balance, int_rate = 0.5):
+    def __init__(self, int_rate, balance,):
         self.int_rate = int_rate
         self.balance = balance
 
@@ -14,26 +10,25 @@ class BankAccount:
         return self
 
     def make_withdraw(self,amount):
-        if BankAccount.can_withdraw(self.balance,amount):
+        if (self.balance - amount) >=0:
             self.balance -=amount
         else:
-            print("Insufficient Funds")
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
         return self
 
     def display_user_balance(self):
-        print(f"{self.name}, balance:{self.balance}")
+        print(f"Balance:{self.balance}")
 
     def yield_interest(self):
         self.balance += (self.balance*self.int_rate)
         return self
 
+account_one = BankAccount(0.01,5000)
+account_two = BankAccount(0.03, 1500)
 
+account_one.make_deposit(500).make_deposit(750).make_deposit(900).make_withdraw(2500)
+account_one.display_user_balance ()
 
-    @classmethod
-    def all_balance(cls):
-        sum = 0
-        for account in cls.all_accounts:
-            sum += account.balance
-        return sum
-
-
+account_two.make_deposit(150).make_withdraw(830).make_withdraw(350).make_withdraw(700)
+account_two.display_user_balance()
