b6defcc1028d89ae53a79448e643bf07c769952f
Ticara <ticarab2005@yahoo.com>
2021-10-27 18:53:08 -0500

    commit

--- P2021_11/Assignments/User_with_Bank_Account/user_with_bank_accounts.py
+++ P2021_11/Assignments/User_with_Bank_Account/user_with_bank_accounts.py
@@ -1,42 +1,52 @@
-class User:
-    def __init__(self, name,):
-        self.name = name        
-        self.account = 0
-
-    def make_deposit(self, amount):
-        self.account +=amount
-        return self
-
-    def make_withdrawal(self,amount):
-        self.account -=amount
-        return self
-
-    def display_user_balance(self):
-        print(f"{self.name}, balance:{self.account_balance}")
-
-
-ticara = User ("Ticara")
-
 class BankAccount:
 
-# don't forget to add some default values for these parameters!
-    def __init__(self, balance,):
+    accounts = []
+    def __init__(self, int_rate, balance,):
+        self.int_rate = int_rate
         self.balance = balance
+        BankAccount.accounts.append(self)
 
     def make_deposit(self, amount):
         self.balance +=amount
         return self
 
+    def make_withdraw(self,amount):
+        if (self.balance - amount) >=0:
+            self.balance -=amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+    
+    def display_account_balance(self):
+        return f"{self.balance}"
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance*self.int_rate)
+        return self
+
+    @classmethod
+    def all_balances(cls):
+        for account in cls.accounts:
+            account.display_account_balance()
+        
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.account = {
+            "savings" : BankAccount (0.03,2500),
+            "checkings" : BankAccount (0.03,2500)
+        }
+
     def display_user_balance(self):
-        print(f"Balance:{self.balance}")
-
-account_one = BankAccount()
-account_two = BankAccount()
-
-account_one.make_deposit(500).make_deposit(750).make_deposit(900).make_withdraw(2500)
-account_one.display_user_balance ()
-
-account_two.make_deposit(150).make_withdraw(830).make_withdraw(350).make_withdraw(700)
-account_two.display_user_balance()
+        print(f"User: {self.name}, Savings Balance:{self.account['savings'].display_account_balance()}")
+        print(f"User: {self.name}, Checkings Balance: {self.account['checkings'].display_account_balance()}")
+        return self     
 
 
+ticara = User ("Ticara")
+
+ticara.account['checkings'].make_withdraw(430)
+ticara.account['savings'].make_deposit(530).make_deposit(700).make_withdraw(925)
+ticara.display_user_balance()
