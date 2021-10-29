5c21b158ca28dfea2e580267311f1b7e85df1257
Ticara <ticarab2005@yahoo.com>
2021-10-29 06:19:53 -0500

    commit

--- P2021_11/Assignments/Playground/server.py
+++ P2021_11/Assignments/Playground/server.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def level1():
     return render_template("index.html",num=3,color="aqua")
 
-@app.route('/play/<int:number>')
-def level2(number):
-    return render_template("index.html",number, color="aqua")
+@app.route('/play/<int:num>')
+def level2(num):
+    return render_template("index.html",num=num, color="aqua")
+
+@app.route('/play/<int:num>/<string:color>')
+def level3(num,color):
+    return render_template ("index.html",num=num,color=color )
 
 if __name__=="__main__":
     app.run(debug=True)
