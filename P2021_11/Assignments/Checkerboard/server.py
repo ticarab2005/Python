b67550824984fbd3ce1d2f18a397727238eda901
Ticara <ticarab2005@yahoo.com>
2021-10-30 21:33:22 -0500

    commit

--- P2021_11/Assignments/Checkerboard/server.py
+++ P2021_11/Assignments/Checkerboard/server.py
@@ -3,10 +3,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',row=8,col=8,box='red',box1='black')
 
-# @app.route('/board')
-#     def 
+
+@app.route('/<int:x>')
+def one(x):
+    return render_template ('index.html',row=x,col=8,box='red',box1='black')
+
+@app.route('/<int:x>/<int:y>')
+def both(x,y):
+    return render_template ('index.html',row=x,col=y,box='red',box1='black')
+
+@app.route('/<int:x>/<int:y>/<string:color>')
+def colors(x,y,color):
+    return render_template('index.html',row=x,col=y,box=color, box1='black')
+
+@app.route('/<int:x>/<int:y>/<string:color>/<string:color1>')
+def alternate(x,y,color,color1):
+    return render_template('index.html',row=x,col=y,box=color, box1=color1)
 
 if __name__ == "__main__":
     app.run(debug=True)
