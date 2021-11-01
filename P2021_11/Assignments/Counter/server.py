c0b2b3230f3afe4e166a69ac31dc91df12d43461
Ticara <ticarab2005@yahoo.com>
2021-10-31 22:30:51 -0500

    commit

--- P2021_11/Assignments/Counter/server.py
+++ P2021_11/Assignments/Counter/server.py
@@ -1,29 +1,26 @@
-from flask import Flask, render_template,request,redirect, session
+from flask import Flask, render_template,redirect, session
 app = Flask(__name__)
-app.secret_key = 'keep it secret, keep it safe'
+app.secret_key = 'Hakuna Matata'
 
 
 @app.route('/')
 def index():
-    if 'click' in session:
-        print('click exists!')
+    if "add" not in session:
+        session["add"] = 0
     else:
-        print("key 'click' does NOT exist")
-    return render_template('index.html')
+        session['add'] += 2
+    return render_template("index.html")
 
-@app.route('/click',methods=["POST"])
-def click():
-    temp_num = 0
-    session["click"].append(temp_num)
-    session.modified = True
-    session["total_clicks"] += 1
-    request redirect('/')
+@app.route('/addition')
+def addition():
+    session['add'] += 0
+    return redirect('/')
 
-@app.route("/destroy_session")
-def clear():
-    session.clear()
-    session.pop('reset')
-    return redirect("/")
+@app.route('/destroy_session')
+def destroy_session():
+    session.clear()	
+    return redirect('/')
+
 
 if __name__=='__main__':
     app.run(debug=True)
