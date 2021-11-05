57a2721aaaac5ffee910f72552ff978220701435
Ticara <ticarab2005@yahoo.com>
2021-11-04 22:53:49 -0500

    commit

--- P2021_11/Assignments/Users_CR/server.py
+++ P2021_11/Assignments/Users_CR/server.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from users import User
+
 app = Flask(__name__)
 app.secret_key = "Chips n dip"
 
+@app.route("/")
+def index():
+    return redirect("/users")
 
+@app.route("/users")
+def users():
+    return render_template("users.html",users=User.get_all())
+
+@app.route("/user/new")
+def new():
+    return render_template("newusers.html")
+
+@app.route("/user/create_user",methods=["POST"])
+def create_user():
+    print(request.form)
+    User.create(request.form)
+    return redirect("/users")
 
 if __name__=="__main__":
     app.run(debug=True)
